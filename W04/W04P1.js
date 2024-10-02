f2929d614d2908bc63d108acffdb6ebd3fb43be9
JasonMerit <Jasonarom@gmail.com>
2024-10-02 09:32:02 +0200

    P1.3 increase and decrease buttons of subdivisions

--- W04/W04P1.js
+++ W04/W04P1.js
@@ -7,6 +7,10 @@
 // To keep consistent across world and eye space, compute the normal matrix as N = (M^T)^-1, where M is just some matrix [15]
 
 
+var pointsArray = [];
+var numTimesToSubdivide = 1;
+
+
 window.onload = function init() {
     let canvas = document.getElementById( "gl-canvas" );
     
@@ -22,55 +26,53 @@ window.onload = function init() {
     var ext = gl.getExtension('OES_element_index_uint');
     if (!ext) { console.log('Warning: Unable to use an extension'); }
 
-    // Create sphere using Tetrahedron
-    var va = vec4(0.0, 0.0, 1.0, 1);
-    var vb = vec4(0.0, 0.942809, -0.333333, 1);
-    var vc = vec4(-0.816497, -0.471405, -0.333333, 1);
-    var vd = vec4(0.816497, -0.471405, -0.333333, 1);
-    let pointsArray = [];
-
-    var numTimesToSubdivide = 5;
-    tetrahedron(pointsArray, va, vb, vc, vd, numTimesToSubdivide);
+    // User interactions
+    document.getElementById("SubdivideButton+").onclick =
+        function() { 
+            numTimesToSubdivide = Math.min(numTimesToSubdivide + 1, 5); 
+            requestAnimationFrame(() => {render(gl)});
+            console.log(numTimesToSubdivide);
+        };
+        
+    document.getElementById("SubdivideButton-").onclick =
+        function() { 
+            numTimesToSubdivide = Math.max(numTimesToSubdivide - 1, 1); 
+            requestAnimationFrame(() => {render(gl)});
+            console.log(numTimesToSubdivide);
+        };
     
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
     
-    // Perspective projection
-    let P = perspective(45.0, 1.0, 0.1, 10.0);  // fovy, aspect (w/h), near, far  (near far are clipping)
-
-    // View
-    let V = lookAt(vec3(0.5, 0.5, -4.5), vec3(0.5, 0.5, 0.0), vec3(0.0, 1.0, 0.0));  // eye, at, look_up
-    
-    render(gl, pointsArray.length, V, P);
+    render(gl);
     
 }   
 
-function tetrahedron(pointsArray, a, b, c, d, n) {
-    divideTriangle(pointsArray, a, b, c, n);
-    divideTriangle(pointsArray, d, c, b, n);
-    divideTriangle(pointsArray, a, d, b, n);
-    divideTriangle(pointsArray, a, c, d, n);
+function tetrahedron(a, b, c, d, n) {
+    divideTriangle(a, b, c, n);
+    divideTriangle(d, c, b, n);
+    divideTriangle(a, d, b, n);
+    divideTriangle(a, c, d, n);
 }
 
 // Midpoint subdivision
-function divideTriangle(pointsArray, a, b, c, count) {
+function divideTriangle(a, b, c, count) {
     if (count > 0) {
         var ab = normalize(mix(a, b, 0.5), true);
         var ac = normalize(mix(a, c, 0.5), true);
         var bc = normalize(mix(b, c, 0.5), true);
-        divideTriangle(pointsArray, a, ab, ac, count - 1);
-        divideTriangle(pointsArray, ab, b, bc, count - 1);
-        divideTriangle(pointsArray, bc, c, ac, count - 1);
-        divideTriangle(pointsArray, ab, bc, ac, count - 1);
+        divideTriangle(a, ab, ac, count - 1);
+        divideTriangle(ab, b, bc, count - 1);
+        divideTriangle(bc, c, ac, count - 1);
+        divideTriangle(ab, bc, ac, count - 1);
     } 
-    else { triangle(pointsArray, a, b, c); }
+    else { triangle(a, b, c); }
 }
 
-function triangle(pointsArray, a, b, c) {
+function triangle(a, b, c) {
     pointsArray.push(a);
     pointsArray.push(b);
     pointsArray.push(c);
@@ -78,22 +80,39 @@ function triangle(pointsArray, a, b, c) {
 
 
 
-function render(gl, num_points, V, P) {
-    // gl.enable( gl.Cull_FACE );  // Add culling for closed 3D objects, that only draws when facing the camera using positive dot product
-    // gl.enable(gl.DEPTH_TEST);  // Add depth test
+function render(gl) {
+    gl.enable( gl.Cull_FACE );  // Add culling for closed 3D objects, that only draws when facing the camera using positive dot product
+    gl.enable(gl.DEPTH_TEST);  // Add depth test
     gl.clearColor(0.3921, 0.5843, 0.9294, 1.0)
-    gl.clear( gl.COLOR_BUFFER_BIT);  // Add depth buffer bit
-    // gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  // Add depth buffer bit
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  // Add depth buffer bit
+
+    // Create sphere using Tetrahedron
+    var va = vec4(0.0, 0.0, 1.0, 1);
+    var vb = vec4(0.0, 0.942809, -0.333333, 1);
+    var vc = vec4(-0.816497, -0.471405, -0.333333, 1);
+    var vd = vec4(0.816497, -0.471405, -0.333333, 1);
+    
+    // Empty the pointsArray
+    pointsArray = [];
+    tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
     // Model
     let M = mat4();
+
+    // View
+    let V = lookAt(vec3(0.5, 0.5, -4.5), vec3(0.5, 0.5, 0.0), vec3(0.0, 1.0, 0.0));  // eye, at, look_up
+
+    // Perspective projection
+    let P = perspective(45.0, 1.0, 0.1, 10.0);  // fovy, aspect (w/h), near, far  (near far are clipping)
+
     
     // First cube one-point
     let MVP = mult(mult(P, V), M);
     let mvp = gl.getUniformLocation(gl.program, "MVP");
     gl.uniformMatrix4fv(mvp, false, flatten(MVP));
 
-    gl.drawArrays(gl.TRIANGLES, 0, num_points);
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
 
     
 }
